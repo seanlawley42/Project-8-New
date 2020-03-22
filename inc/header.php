abbe922e664d01df8f82e991e66babe01641ad45
paperclipmonger <seanlawley42@gmail.com>
2020-03-22 18:14:21 -0400

    Peer Review Edits

--- inc/header.php
+++ inc/header.php
@@ -48,19 +48,24 @@
   		<h1>
         <a class="logo" href="./">
           <svg viewbox="0 0 64 64" class="logo-icon"><use xlink:href="#logo_icon"></use></svg>
-          <span class="logo-name">Personal Todo App</span>
+          <span class="logo-name">Wayne Enterprises Todo App</span>
         </a>
       </h1>
 
         <ul class="nav navbar-left">
+        <?php if (isAuthenticated()) : ?>
         <li class="nav-item tasks<?php if ($page == "tasks") { echo " on"; } ?>"><a class="nav-link" href="task_list.php">View Tasks</a></li>
         <li class="nav-item task<?php if ($page == "task") { echo " on"; } ?>"><a class="nav-link" href="task.php">Add Tasks</a></li>
+        <?php endif; ?> 
       </ul>
         <ul class="nav">
+        <?php if (isAuthenticated()) : ?>
             <li class="nav-item task<?php if ($page == "account") { echo " on"; } ?>"><a class="nav-link" href="/account.php">My Account</a></li>
             <li class="nav-item tasks"><a class="nav-link" href="/procedures/doLogout.php">Logout</a></li>
-            <li class="nav-item tasks<?php if ($page == "login") { echo " on"; } ?>"><a class="nav-link" href="//login.php">Login</a></li>
+        <?php else : ?>
+            <li class="nav-item tasks<?php if ($page == "login") { echo " on"; } ?>"><a class="nav-link" href="/login.php">Login</a></li>
             <li class="nav-item tasks<?php if ($page == "register") { echo " on"; } ?>"><a class="nav-link" href="/register.php">Register</a></li>
+          <?php endif; ?>
         </ul>
     </div>
   </header>
